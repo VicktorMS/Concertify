c1049e7e417b4ec2b258c97e74d4a558e08a5c4d
Victor Moraes <victor.moraesrj@gmail.com>
2023-04-18 11:57:37 -0300

    Estilização com Tema e fonte desnecessária excluida

--- src/pages/GetStarted/GetStarted.jsx
+++ src/pages/GetStarted/GetStarted.jsx
@@ -7,36 +7,51 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Logo from '/src/components/Logo/Logo';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+ const spotifyTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1AB26B',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+    background: {
+      default: '#000000',
+      paper: '#2f2f2f',
+    },
+  },
+});
 
 export default function GetStarted() {
   return (
-    <Box sx={{ 
-      backgroundColor: '#000000', 
-      width: '100%', 
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: '0'
-      }}>
-      <CssBaseline />
-      <Stack direction="row" justifyContent="center" alignItems="center" flexWrap="wrap"spacing={0}>
-        <Logo/>
-        <Typography variant="h3" sx={{
-          textAlign: 'right',
-          width: '500px',
-          maxWidth: '80vw'
-        }} >
-        Encontre shows de seus artistas
-
-        {/* Alterar  */}
-        FAVORITOS
-        </Typography>
-      </Stack>
-      <Button variant="contained">
-        <Link to="/home">Get Started</Link>
-      </Button>
-    </Box>
+    <ThemeProvider theme={spotifyTheme}>
+      <Box sx={{ 
+        width: '100%', 
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '0'
+        }}>
+        <CssBaseline />
+        <Stack direction="row" justifyContent="center" alignItems="center" flexWrap="wrap"spacing={0}>
+          <Logo/>
+          <Typography variant="h3" sx={{
+            textAlign: 'right',
+            width: '500px',
+            maxWidth: '80vw'
+          }} >
+          Encontre shows de seus artistas
+          </Typography>
+        </Stack>
+        <Button variant="contained">
+          <Link to="/home">Get Started</Link>
+        </Button>
+      </Box>
+    </ThemeProvider>
   )
 }
