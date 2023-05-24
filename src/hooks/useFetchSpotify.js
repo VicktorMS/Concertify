1d3787b8a116529cfb0e3622b29a7f841b7d402b
Victor Moraes <victor.moraesrj@gmail.com>
2023-05-23 21:43:49 -0300

    Consumo de Api Spotify

---
+++ src/hooks/useFetchSpotify.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(null);
+
+  const baseUrl = "https://rest.bandsintown.com/";
+  const secretBandsInTown = "?app_id=b44ac6574df8cdc7291e1e543bea0a67";
+
+  useEffect(() => {
+    fetch(`${baseUrl}${url}${secretBandsInTown}`)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
+      .finally(() => setIsFetching(false));
+  }, [url]);
+
+  return { data, error, isFetching };
+}
