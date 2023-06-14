49dd4eb380dfe9f8d3bd675599e06ab3d70a629c
Victor Moraes <victor.moraesrj@gmail.com>
2023-06-14 17:06:38 -0300

    Faq text

--- src/components/FAQButton/FAQButton.jsx
+++ src/components/FAQButton/FAQButton.jsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import Typography from "@mui/material/Typography";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
+  "& .MuiDialogContent-root": {
     padding: theme.spacing(2),
   },
-  '& .MuiDialogActions-root': {
+  "& .MuiDialogActions-root": {
     padding: theme.spacing(1),
   },
 }));
@@ -31,7 +31,7 @@ function BootstrapDialogTitle(props) {
           aria-label="close"
           onClick={onClose}
           sx={{
-            position: 'absolute',
+            position: "absolute",
             right: 8,
             top: 8,
             color: (theme) => theme.palette.grey[500],
@@ -61,33 +61,43 @@ export default function CustomizedDialogs() {
 
   return (
     <div>
-      <HelpOutlineIcon onClick={handleClickOpen}/>
+      <HelpOutlineIcon onClick={handleClickOpen} />
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleClose}
+        >
           Ajuda e Contato
         </BootstrapDialogTitle>
         <DialogContent dividers>
           <Typography gutterBottom>
-            Esse projeto foi desenvolvido como projeto de bloco
+            Este projeto foi desenvolvido como parte do projeto de bloco pelos
+            alunos do Instituto INFNET.
           </Typography>
           <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+            Se você encontrar algum problema com o funcionamento do aplicativo,
+            pedimos que entre em contato conosco por meio da opção "Enviar
+            Email" e descreva detalhadamente o problema que está enfrentando.
+            Faremos o possível para resolver o problema o mais rápido possível.
           </Typography>
           <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-            ullamcorper nulla non metus auctor fringilla.
+            Autores do projeto: Alexsandro Salles, Ruan Campelo, William Piezeta
+            e Victor de Moraes.
+          </Typography>
+          <Typography gutterBottom>
+            Agradecemos seu interesse em nosso aplicativo e estamos ansiosos
+            para receber seu feedback.
           </Typography>
         </DialogContent>
         <DialogActions>
-          <Button 
-          autoFocus onClick={handleClose}
-          href="mailto:victor.moraesrj@gmail.com?subject=Assunto do email&cc=victor.moraesrj@gmail.com?&bcc=victor.moraesrj@gmail.com?&body=Conteúdo do email que será preenchido automaticamente"
+          <Button
+            autoFocus
+            onClick={handleClose}
+            href="mailto:victor.moraesrj@gmail.com?subject=Assunto do email&cc=victor.moraesrj@gmail.com?&bcc=victor.moraesrj@gmail.com?&body=Conteúdo do email que será preenchido automaticamente"
           >
             Enviar Email
           </Button>
